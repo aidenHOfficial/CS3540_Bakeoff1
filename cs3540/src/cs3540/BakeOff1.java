e0109f135c39d910dd5d769cbc628f335d1d3699
aidenHOfficial <aiden.b.humphri3s@gmail.com>
2025-10-14 10:42:15 -0600

    Changed the base code to write to a local csv file, and modified button size

--- cs3540/src/cs3540/BakeOff1.java
+++ cs3540/src/cs3540/BakeOff1.java
@@ -4,41 +4,89 @@ import java.awt.AWTException;
 import java.awt.List;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.imageio.ImageIO;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
+
 import processing.core.PApplet;
 import processing.core.PImage;
 import java.awt.Point;
 
 public class BakeOff1 extends PApplet {
-	// when in doubt, consult the Processsing reference:
-	// https://processing.org/reference/
-	// The argument passed to main must match the class name
-	public static void main(String[] args) {
-		// Tell processing what class we want to run.
-		PApplet.main("cs3540.BakeOff1");
-	}
-
-	int margin = 200; // set the margin around the squares
 	final int padding = 50; // padding between buttons and also their width/height
 	final int buttonSize = 40; // padding between buttons and also their width/height
-	ArrayList<Integer> trials = new ArrayList<Integer>(); // contains the order of buttons that activate in the test
+	final int selectedButtonSize = 140;
+	int margin = 200; // set the margin around the squares
+	int windowSize = 700;
 	int trialNum = 0; // the current trial number (indexes into trials array above)
 	int startTime = 0; // time starts when the first click is captured
 	int finishTime = 0; // records the time of the final click
+	int timeTaken = 0;
 	int hits = 0; // number of successful clicks
-	int misses = 0; // number of missed clicks
+	static int misses = 0; // number of missed clicks
+	static int numRepeats = 10; // sets the number of times each button repeats in the test
+	static int allowedMisses = 0;
+	static int id = 0; // identifies user of group 28 0=Aiden 1=Bianca 2=Koosha
+	
+	boolean failed = false;
+	boolean fininshed = false;
+	
+	ArrayList<Integer> trials = new ArrayList<Integer>(); // contains the order of buttons that activate in the test
+
+	String csvData = "";
+	
 	Robot robot; // initialized in setup
-	int windowSize = 700;
 	
 	PImage arrowImage;
+	
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    String name = "";
+	    	
+	    System.out.println("Welcome to the test. The button size has increased significantly, and there is a new rule:\n\tYour accuracy at the end of the test must be >= 99%. If it dips below this theoretical best, the test will end prematurely.\n\nGood Luck!");
+	    while (!(name.equals("aiden") || name.equals("bianca") || name.equals("koosha") || name.equals("exit"))) {
+	        System.out.println("Enter your name: (Aiden, Bianca, Koosha, or \"Exit\" to leave)");
+	        name = scanner.nextLine().trim().toLowerCase();
+	    }	    
+	    scanner.close();
 
-	int numRepeats = 1; // sets the number of times each button repeats in the test
+	    switch (name) {
+	        case "aiden":
+	            id = 0;
+	            break;
+	        case "bianca":
+	            id = 1;
+	            break;
+	        case "koosha":
+	            id = 2;
+	            break;
+	        case "exit":
+	        	return;
+	    }
+	    
+		int totalTrials = 16 * numRepeats - 1;
+	    allowedMisses = -((99 * totalTrials)/(100))+totalTrials-1;
+
+	    PApplet.main("cs3540.BakeOff1");
+	}
+	
+	public void writeToCSV(String s) {
+        String csvFile = "preformance.csv";
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(csvFile, true))) {
+        	writer.println(s);
+        } 
+        catch (IOException e) {
+            System.err.println("Error writing to CSV file: " + e.getMessage());
+        }
+	}
 
 	/**
 	 * https://processing.org/reference/settings_.html#:~:text=The%20settings()%20method%20runs,commands%20in%20the%20Processing%20API.
@@ -51,48 +99,39 @@ public class BakeOff1 extends PApplet {
 	 * // https://processing.org/reference/setup_.html
 	 */
 	public void setup() {
-		// noCursor(); // hides the system cursor if you want
-		noStroke(); // turn off all strokes, we're just using fills here (can change this if you
-					// want)
-		textFont(createFont("Arial", 16)); // sets the font to Arial size 16
+		noStroke(); 
+		textFont(createFont("Arial", 16));
 		textAlign(CENTER);
-		frameRate(60); // normally you can't go much higher than 60 FPS.
-		ellipseMode(CENTER); // ellipses are drawn from the center (BUT RECTANGLES ARE NOT!)
-		// rectMode(CENTER); //enabling will break the scaffold code, but you might find
-		// it easier to work with centered rects
+		frameRate(60);
+		ellipseMode(CENTER);
 
 		try {
-			robot = new Robot(); // create a "Java Robot" class that can move the system cursor
+			robot = new Robot();
 		} catch (AWTException e) {
 			e.printStackTrace();
 		}
 
-		// ===DON'T MODIFY MY RANDOM ORDERING CODE==
-		for (int i = 0; i < 16; i++) // generate list of targets and randomize the order
-			// number of buttons in 4x4 grid
+		for (int i = 0; i < 16; i++)
 			for (int k = 0; k < numRepeats; k++)
-				// number of times each button repeats
 				trials.add(i);
 
-		Collections.shuffle(trials); // randomize the order of the buttons
-		System.out.println("trial order: " + trials); // print out order for reference
+		Collections.shuffle(trials);
+		System.out.println("trial order: " + trials);
 
-		surface.setLocation(0, 0);// put window in top left corner of screen (doesn't always work)
+		surface.setLocation(0, 0);
 		
-		arrowImage = loadImage("arrow.png"); // Replace with your image file
+		arrowImage = loadImage("arrow.png");
 		imageMode(CENTER);
-		arrowImage.resize(buttonSize-10, buttonSize-10);
+		arrowImage.resize(buttonSize-10, buttonSize-10);        
 	}
 
 	public void draw() {
-		background(0); // set background to black
+		background(0);
 
-		if (trialNum >= trials.size()) // check to see if test is over
+		if (trialNum >= trials.size())
 		{
 			float timeTaken = (finishTime - startTime) / 1000f;
-			float penalty = constrain(((95f - ((float) hits * 100f / (float) (hits + misses))) * .2f), 0, 100);
-			fill(255); // set fill color to white
-			// write to screen (not console)
+			fill(255);
 			text("Finished!", width / 2, height / 2);
 			text("Hits: " + hits, width / 2, height / 2 + 20);
 			text("Misses: " + misses, width / 2, height / 2 + 40);
@@ -100,71 +139,90 @@ public class BakeOff1 extends PApplet {
 			text("Total time taken: " + timeTaken + " sec", width / 2, height / 2 + 80);
 			text("Average time for each button: " + nf((timeTaken) / (float) (hits + misses), 0, 3) + " sec", width / 2,
 					height / 2 + 100);
-			text("Average time for each button + penalty: "
-					+ nf(((timeTaken) / (float) (hits + misses) + penalty), 0, 3) + " sec", width / 2,
-					height / 2 + 140);
-			return; // return, nothing else to do now test is over
+			text("Successfully written scores to shared csv!", width / 2, height / 2 + 120);
+			
+			if (!finished) {
+				finished = true;
+				writeToCSV(csvData);
+			}
+
+			return;
+		}
+		if (failed) {
+			fill(255);
+			text("Accuracy went below 99%. Test failed!", width / 2, height / 2);
+			return;
 		}
 
-		fill(255); // set fill color to white
-		text((trialNum + 1) + " of " + trials.size(), 40, 20); // display what trial the user is on
-
+		fill(255);
 		drawOmbreBackground();
+		
+	    text("Allowed misses: " + allowedMisses, 75, 40);
+		text((trialNum + 1) + " of " + trials.size(), 40, 20); // display what trial the user is on
 		
 		for (int i = 0; i < 16; i++) {
 			drawOmbreButton(i);
 			drawArrowButton(i);
 		}
-		
-		fill(255, 0, 0, 200); // set fill color to translucent red
-		ellipse(mouseX, mouseY, 20, 20); // draw user cursor as a circle with a diameter of 20
-
 	}
 
 	public void mousePressed() // test to see if hit was in target!
-	{
-		if (trialNum >= trials.size()) // check if task is done
+	{	
+		if (trialNum >= trials.size() || failed) // check if task is done
 			return;
 
-		if (trialNum == 0) // check if first click, if so, record start time
+		if (trialNum == 0)
 			startTime = millis();
 
 		if (trialNum == trials.size() - 1) // check if final click
 		{
 			finishTime = millis();
-			// write to terminal some output:
 			System.out.println("we're all done!");
 		}
 
 		Rectangle bounds = getButtonLocation(trials.get(trialNum));
-
-		// check to see if cursor was inside button
-		if ((mouseX > bounds.x && mouseX < bounds.x + bounds.width)
-				&& (mouseY > bounds.y && mouseY < bounds.y + bounds.height)) // test to see if hit was within bounds
-		{
-			System.out.println("HIT! " + trialNum + " " + (millis() - startTime)); // success
-			hits++;
-		} else {
-			System.out.println("MISSED! " + trialNum + " " + (millis() - startTime)); // fail
-			misses++;
+		
+		boolean inBounds = ((mouseX > bounds.x && mouseX < bounds.x + bounds.width)
+				&& (mouseY > bounds.y && mouseY < bounds.y + bounds.height));
+		
+		timeTaken = millis() - timeTaken;
+		
+		String csvString = trialNum + "," + id + "," + mouseX + "," +  mouseY + "," +  bounds.getCenterX() + "," +  bounds.getCenterY() + "," +  selectedButtonSize + "," +  timeTaken + "," +  inBounds + "\n";
+		System.out.println(csvString);
+		
+		if (trialNum > 0) { // First trial doesn't count to the total
+			csvData += csvString;
+			
+			timeTaken = millis();
+			
+			if (inBounds)
+			{
+				hits++;
+			} 
+			else {
+				misses++;
+				allowedMisses--;
+				
+				int totalTrials = (16 * numRepeats) - 1;
+				if (((float) (totalTrials - misses) * 100f / (float) totalTrials) < 99){
+					failed = true;
+				}
+			}
 		}
 
-		trialNum++; // Increment trial number
 
-		// in this example design, I move the cursor back to the middle after each click
-		// Note. When running from eclipse the robot class affects the whole screen not
-		// just the GUI, so the mouse may move outside of the GUI.
-//		robot.mouseMove(width/2, (height)/2); //on click, move cursor to roughly
-		// center of window!
+		trialNum++;
 	}
 
 	// probably shouldn't have to edit this method
-	public Rectangle getButtonLocation(int i) // for a given button ID, what is its location and size
-	{
-		int x = (i % 4) * (padding + buttonSize) + margin;
-		int y = (i / 4) * (padding + buttonSize) + margin;
+	public Rectangle getButtonLocation(int i) {
+	    int baseX = (i % 4) * (padding + buttonSize) + margin;
+	    int baseY = (i / 4) * (padding + buttonSize) + margin;
+	    int size = (i == trials.get(trialNum)) ? selectedButtonSize : buttonSize;
 
-		return new Rectangle(x, y, buttonSize, buttonSize);
+	    // Offset to keep larger button centered
+	    int offset = (buttonSize - size) / 2;
+	    return new Rectangle(baseX + offset, baseY + offset, size, size);
 	}
 	
 	public void drawButton(int i) {
@@ -181,7 +239,7 @@ public class BakeOff1 extends PApplet {
 	public void drawOmbreBackground() {
 		Rectangle targetBounds = getButtonLocation(trials.get(trialNum));
 		
-		double startDist = Math.sqrt(2 * Math.pow(buttonSize, 2));
+		double startDist = Math.sqrt(2 * Math.pow(selectedButtonSize, 2));
 		
 		Rectangle topLeftLocation = getButtonLocation(0);
 		Rectangle topRightLocation = getButtonLocation(3);
@@ -230,9 +288,9 @@ public class BakeOff1 extends PApplet {
 	    double greenStep = (endColor[1] - startColor[1]) / (double)steps;
 	    double blueStep  = (endColor[2] - startColor[2]) / (double)steps;
 
-	    double segmentDist = (endDist - 100) / steps;
-	    double targetXCenter = xTarget + (buttonSize / 2.0);
-	    double targetYCenter = yTarget + (buttonSize / 2.0);
+	    double segmentDist = (endDist) / steps;
+	    double targetXCenter = xTarget + (selectedButtonSize / 2.0);
+	    double targetYCenter = yTarget + (selectedButtonSize / 2.0);
 
 	    PImage ombreImage = new PImage(imageSize, imageSize, ARGB);
 
@@ -265,6 +323,10 @@ public class BakeOff1 extends PApplet {
 	public void drawOmbreButton(int i) {
 	    Rectangle bounds = getButtonLocation(i);
 	    Rectangle targetBounds = getButtonLocation(trials.get(trialNum));
+	    int size = buttonSize;
+	    if (trials.get(trialNum) == i) {
+	    	size = selectedButtonSize;
+	    }
 
 	    double startDist = Math.sqrt(2 * Math.pow(buttonSize, 2));
 
@@ -291,7 +353,7 @@ public class BakeOff1 extends PApplet {
 	    PImage buttonImage;
 	    try {
 	        buttonImage = generateOmbreButtonImage(bounds.x, bounds.y, targetBounds.x, targetBounds.y, 
-	                                               20, startDist, endDist, buttonSize);
+	                                               20, startDist, endDist, size);
 	    } catch (Exception e) {
 	        return;
 	    }
@@ -310,13 +372,12 @@ public class BakeOff1 extends PApplet {
 	    Rectangle bounds = getButtonLocation(i);
 	    Rectangle targetBounds = getButtonLocation(trials.get(trialNum));
 
-	    double relativeX = bounds.x - targetBounds.x;
-	    double relativeY = bounds.y - targetBounds.y;
+	    double relativeX = bounds.getCenterX() - targetBounds.getCenterX();
+	    double relativeY = bounds.getCenterY() - targetBounds.getCenterY();
 
 	    float ang = (float) ((180 * Math.atan2(relativeY, relativeX)) / Math.PI) - 180;
 
 	    if (trials.get(trialNum) != i) {
-	        // Just draw the arrow over the ombre
 	        drawImage(arrowImage, bounds.x + (bounds.width / 2), bounds.y + (bounds.height / 2), ang);
 	    }
 	}
